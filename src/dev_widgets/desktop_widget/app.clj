5f892dcb1b1ffc76495c2bf87b48f3815a36b128
Thomas Sojka <thsojka@web.de>
2022-10-31 07:53:58 +0100

    Color prototype

--- src/dev_widgets/desktop_widget/app.clj
+++ src/dev_widgets/desktop_widget/app.clj
@@ -1,14 +1,12 @@
 (ns dev-widgets.desktop-widget.app
   (:require [cljfx.css :as css]
-            [com.evocomputing.colors :as colors]
             [cljfx.dev :as cdev]
-            [dev-widgets.desktop-widget.renderer :refer [reload]]
-            [dev-widgets.desktop-widget.core :refer [*state]]))
+            [com.evocomputing.colors :as colors]))
 
 (def style
   (css/register ::style
-                {".test-gradient" {:-fx-fill "linear-gradient(to bottom, #ff00ff, #6a5acd)"}
-                 ".rainbow-gradient" {:-fx-fill "linear-gradient(to right, #f00 0%, #ff0 17%, #0f0 33%, #0ff 50%, #00f 67%, #f0f 83%, #f00 100%)"}
+                {".test-color" {:-fx-fill "#FF00E4"}
+                 ".rainbow-gradient" {:-fx-fill "linear-gradient(to right, #ff0000 0%, #ff0 17%, #0f0 33%, #0ff 50%, #00f 67%, #f0f 83%, #f00 100%)"}
                  ".bg-slate-400" {:-fx-fill "rgb(148,163,184)"}}))
 
 (defn color-prop [{:keys [label value]}]
@@ -24,13 +22,7 @@
      :on-text-changed prn
      :pref-width 40}]})
 
-(defn mouse-pos []
-  (let [point (.. java.awt.MouseInfo getPointerInfo getLocation)]
-    [(.getX point) (.getY point)]))
-
-(def start-pos (mouse-pos))
-
-(defn root-view [{{:keys [color color-slider-position]} :state}]
+(defn root-view [{:keys [color color-slider-position start-pos position path]}]
   (let [[x y] start-pos
         color (colors/create-color color)]
     {:fx/type :stage
@@ -41,14 +33,16 @@
      :scene {:fx/type :scene
              :stylesheets [(::css/url style)]
              :root {:fx/type :v-box
-                    :children [{:fx/type :rectangle
-                                :width 160
-                                :height 100
-                                :style-class "test-gradient"}
-                               {:fx/type :pane
-                                :on-mouse-dragged (fn [e]
-                                                    (swap! *state assoc :color-slider-position (min (- 160 10) (max 0 (.getX e)))))
+                    :children [{:fx/type :pane
+                                :on-mouse-dragged {:event/type :update-color :color color :position position :path path}
+                                :on-mouse-clicked {:event/type :update-color :color color :position position :path path}
                                 :children [{:fx/type :rectangle
+                                            :arc-height 10
+                                            :arc-width 10
+                                            :width 160
+                                            :height 160
+                                            :style-class "test-color"}
+                                           {:fx/type :rectangle
                                             :arc-height 10
                                             :arc-width 10
                                             :width 160
@@ -64,7 +58,6 @@
                                {:fx/type :h-box
                                 :children [(color-prop {:label "H" :value (colors/hue color)})
                                            (color-prop {:label "S" :value (colors/saturation color)})
-                                           (color-prop {:label "L" :value (colors/lightness color)})
-                                           (color-prop {:label "A"})]}]}}}))
-(comment
-  (reload))
+                                           (color-prop {:label "L" :value (colors/lightness color)})]}]}}}))
+
+#_(reload)
--- src/dev_widgets/desktop_widget/core.clj
+++ src/dev_widgets/desktop_widget/core.clj
@@ -1,13 +1,89 @@
 (ns dev-widgets.desktop-widget.core
   (:gen-class)
   (:require
+   [babashka.fs :as fs]
    [cljfx.api :as fx]
-   [dev-widgets.desktop-widget.renderer :refer [renderer]]
-   [dev-widgets.desktop-widget.source-file :as source-file]))
+   [com.evocomputing.colors :as colors]
+   [dev-widgets.desktop-widget.app :refer [root-view]]))
 
 (defonce *state
   (atom {:color nil}))
 
+(defn read-value [path pos]
+  (let [start (dec pos)
+        end (+ start 7)
+        path  path
+        bytes (vec (fs/read-all-bytes path))]
+    (->> (subvec bytes start end)
+         (map char)
+         (apply str))))
+
+(defn write-value [path pos value]
+  (let [start (dec pos)
+        end (+ start 7)
+        [first-line] (fs/read-all-lines path)
+        ;; TODO use a proper parser instead of a regexp
+        [_ ns] (re-find #"\(ns ([\w\-.]*)" first-line)
+        bytes (vec (fs/read-all-bytes path))]
+    (->> (reduce into [(subvec bytes 0 start)
+                       (mapv int value)
+                       (subvec bytes end)])
+         byte-array
+         (fs/write-bytes path))
+    (use (symbol ns) :reload)))
+
+(defn interpolate [x [from-domain to-domain] [from-range to-range]]
+  (float
+   (* (if (zero? x) 0 (/ x (- to-domain from-domain)))
+      (- to-range from-range))))
+
+
+(defn map-event-handler [event]
+  (case (:event/type event)
+    :update-color
+    (fn [state]
+      (let [position (min (- 160 10) (max 0 (.getX (:fx/event event))))
+            color (:color event)
+            hue (interpolate position [0 150] [0 359])
+            new-color (colors/rgb-hexstr (colors/create-color
+                                          {:h hue
+                                           :s (colors/saturation color)
+                                           :l (colors/lightness color)}))]
+        (write-value
+         (:path event)
+         (:position event)
+         new-color)
+        (assoc state
+               :color-slider-position position
+               :color new-color)))))
+
+(defn renderer []
+  (fx/create-renderer
+    :middleware (fx/wrap-map-desc assoc :fx/type root-view)
+    :opts {:fx.opt/map-event-handler #(swap! *state (map-event-handler %))}))
+
+(defn reload []
+  (renderer))
+
+(defn add
+  ([v] v)
+  ([[x1 y1] [x2 y2]]
+   [(+ x1 x2) (+ y1 y2)])
+  ([v1 v2 & vs]
+   (apply add (add v1 v2) vs)))
+
 (defn -main [& args]
-  (swap! *state assoc :color (source-file/read-value "/Users/thomas/projects/dev-widgets/src/dev_widgets/desktop_widget/app.clj" 389))
-  (fx/mount-renderer *state renderer))
+  (let [[path position start-pos] args]
+    (swap! *state assoc
+           :color (read-value path position)
+           :position position
+           :path path
+           :color-slider-position 0
+           :start-pos (add start-pos [10 20])))
+  (fx/mount-renderer *state (renderer)))
+
+(comment
+  (-main
+   "/Users/thomas/projects/dev-widgets/src/dev_widgets/desktop_widget/app.clj"
+   231)
+  (dev-widgets.desktop-widget.core/-main "/Users/thomas/projects/dev-widgets/src/dev_widgets/desktop_widget/app.clj" 231))
