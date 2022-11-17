1b5983bb80a03c6924bfcc321a3d47b3e301e4b4
Thomas Sojka <thomas.sojka@comsysto.com>
2022-11-17 14:36:05 +0100

    Use nrepl to start dev widgets

---
+++ scripts/dev_widget.clj
@@ -0,0 +1,13 @@
+(ns dev-widget
+  (:require [babashka.nrepl-client :as nrepl]
+            [clojure.tools.cli :as cli]))
+
+(def cli-options [["-pos" "--position POSITION" "Position"
+                   :parse-fn #(Integer/parseInt %)]
+                  ["-s" "--start-pos START-POSITION" "Start Position"
+                   :parse-fn #(read-string %)]
+                  ["-p" "--path PATH" "Path" ]])
+(defn start! []
+  (let [{:keys [position path start-pos]} (:options  (cli/parse-opts *command-line-args* cli-options))]
+    (nrepl/eval-expr {:port 7888 :expr
+                      (str "(dev-widgets.desktop-widget.core/start! {:position " position " :path \"" path "\" :start-pos " start-pos "})")})))
--- src/dev_widgets/desktop_widget/core.clj
+++ src/dev_widgets/desktop_widget/core.clj
@@ -1,43 +1,26 @@
 (ns dev-widgets.desktop-widget.core
-  (:gen-class)
   (:require [cljfx.api :as fx]
             [dev-widgets.desktop-widget.app :refer [root-view]]
             [dev-widgets.desktop-widget.events :as events]
             [dev-widgets.desktop-widget.fs :as fs]
-            [dev-widgets.desktop-widget.util :as util]))
+            [dev-widgets.desktop-widget.util :as util]
+            [nrepl.server :as nrepl]))
+
+(defonce server (nrepl/start-server :port 7888))
 
 (def *state
   (atom {:color-slider-position 0}))
 
-(def init
-  {:cursor-position 321
-   :path "/Users/thomas/projects/dev-widgets/src/dev_widgets/desktop_widget/app.clj"
-   :cursor-coord [2809 338]})
-
 (defn renderer []
   (fx/create-renderer
-   :middleware (fx/wrap-map-desc (fn [state]
-                                   (merge {:fx/type root-view}
-                                          (let [{:keys [path cursor-position cursor-coord]} init]
-                                            {:position cursor-position
-                                             :path path
-                                             :start-pos (util/add cursor-coord [10 20])
-                                             :color (fs/read-value path cursor-position)})
-                                          state)))
+   :middleware (fx/wrap-map-desc (fn [state] (merge {:fx/type root-view} state)))
    :opts {:fx.opt/map-event-handler #(swap! *state (events/handler %))}))
 
-(defn -main [& args]
-  (let [[path position start-pos] args]
-    (swap! *state assoc
-           :color (fs/read-value path position)
-           :position position
-           :path path
-           :color-slider-position 0
-           :start-pos (util/add start-pos [10 20])))
+(defn start! [{:keys [position path start-pos]}]
+  (swap! *state assoc
+         :color (fs/read-value path position)
+         :position position
+         :path path
+         :color-slider-position 0
+         :start-pos (util/add start-pos [10 20]))
   (fx/mount-renderer *state (renderer)))
-
-(comment
-  (-main
-   "/Users/thomas/projects/dev-widgets/src/dev_widgets/desktop_widget/app.clj"
-   231)
-  (dev-widgets.desktop-widget.core/-main "/Users/thomas/projects/dev-widgets/src/dev_widgets/desktop_widget/app.clj" 231))
